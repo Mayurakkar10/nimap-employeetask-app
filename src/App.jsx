1bd24f909a3ef83076b91f34a17eb7e16bd351bd
Mayur Akkar <91078024+Mayurakkar10@users.noreply.github.com>
2025-01-03 08:36:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Employee from "./Components/Employee";
 import "/node_modules/bootstrap/dist/css/bootstrap.min.css";
-import EmployeeManagementSystem from "./Components/NewComponent";
+import EmployeeManagementSystem from "./Components/EmployeeManagementSystem";
 export default function App() {
   return (
     <div>
